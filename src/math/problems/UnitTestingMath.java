d35cd2d50c5564f8ff0ab084588309725f9b3755
faridgraiche <farid.graiche150@gmail.com>
2022-01-21 18:37:19 -0500

    math

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -4,26 +4,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UnitTestingMath {
-    public static void main(String[] args) {
-        //Apply Unit testing into each classes and methods in this package.
+
+    @Test
+    public void setExpectedFactorial() {
 
         Factorial factorial = new Factorial();
         int actualFactorial;
         int expectedFactorial = 120;
         actualFactorial = 120;
-        Assert.assertEquals(expectedFactorial,actualFactorial);
+        Assert.assertEquals(expectedFactorial, actualFactorial);
         System.out.println("test 1 passed");
+    }
 
 
+    @Test
+    public void tsetFibonacci(){
 
-        Fibonacci fibonacci = new Fibonacci();
-        boolean actualresult;
-        boolean expectedresult = true;
-        actualresult = true;
+    Fibonacci fibonacci = new Fibonacci();
+    boolean actualresult;
+    boolean expectedresult = true;
+    actualresult =true;
         Assert.assertEquals(expectedresult,actualresult);
         System.out.println(" correct");
+}
 
-
+    @Test
+            public void setLowestNumber() {
 
         LowestNumber lowestNumber = new LowestNumber();
         int actualResult;
@@ -32,55 +38,62 @@ public class UnitTestingMath {
 
         Assert.assertEquals(expectedResult, actualResult);
         System.out.println("test passed");
-
-
+    }
+    @Test
+            public void setFindLowestDifference() {
         FindLowestDifference findLowestDifference = new FindLowestDifference();
         int actual;
         int expected = 1;
-        actual=1;
+        actual = 1;
 
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
         System.out.println("test passed");
+    }
 
+    @Test
+            public void setMakePyramid() {
 
 
         MakePyramid makePyramid = new MakePyramid();
         String Actual;
-        String  Expected = "pyramid";
+        String Expected = "pyramid";
         Actual = "pyramid";
 
-        Assert.assertEquals(Actual,Expected);
+        Assert.assertEquals(Actual, Expected);
         System.out.println("test 5 passed");
-
-
+    }
+    @Test
+            public void setPrimeNumber() {
 
         PrimeNumber primeNumber = new PrimeNumber();
         boolean Result;
         boolean expect = true;
         Result = true;
 
-        Assert.assertEquals(Result,expect);
+        Assert.assertEquals(Result, expect);
         System.out.println("test 6 passed");
 
-
+    }
+    @Test
+            public void setFindMissingNumber() {
 
         FindMissingNumber findMissingNumber = new FindMissingNumber();
         int result;
         int Expect = 9;
         result = 9;
 
-        Assert.assertEquals(result,Expect);
+        Assert.assertEquals(result, Expect);
         System.out.println("test 7 passed");
 
 
-
-
-
-
-
-
-
     }
+
+
+
+
+
+
+
 
 
 
